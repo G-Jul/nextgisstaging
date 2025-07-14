9eb2296eaadda0c3e9f778e1166180104b74f949
Yulia <grigorenko.j@gmail.com>
2025-07-14 17:25:12 +0300

    Update conf.py
    alternative home page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+html_additional_pages = {"index": "index_modified.html"}
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
