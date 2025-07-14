f84adca497607585021fcf1e450ddb70a7997422
Yulia <grigorenko.j@gmail.com>
2025-07-14 17:37:21 +0300

    Update conf.py
    alt page to rtd_index_modified

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,7 @@ extensions = [
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-html_additional_pages = {"index": "index_modified.html"}
+html_additional_pages = {"index": "rtd_index_modified.html"}
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
