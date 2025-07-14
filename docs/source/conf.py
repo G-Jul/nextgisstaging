4433855793d98f3103283ddfadf8209c4220c9a4
Yulia <grigorenko.j@gmail.com>
2025-07-14 15:23:48 +0300

    Update conf.py
    background green

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#7C4DFF",
         "color-brand-content": "#7C4DFF",
+        "color-background-primary": green
     },
 }
 
