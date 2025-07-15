672fb76d5579465d57ca26fb64fe85ed50ab6a75
Yulia <grigorenko.j@gmail.com>
2025-07-15 12:21:09 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,6 @@ html_theme_options = {
         "color-header-text": "#FFFFFF",
         "color-sidebar-background": "#bcd3e6",
         "color-sidebar-brand-text": "#0c65af",
-        "font-size--normal": 25px;
-        "admonition-font-size" 20px:
     },
 }
 
