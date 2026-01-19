168542a7d9dbdcb8b46d81fb188fc0a06719a165
nazar <nazaralexx2@mail.ru>
2026-01-19 19:29:39 +0900

    Fix: use custom index.html instead of index_rtd.html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-html_additional_pages = {"index": "index_rtd.html"}
+html_additional_pages = {"index": "index.html"}
 html_static_path = ['_static']
 
 # -- Options for HTML output
